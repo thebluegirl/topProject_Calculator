20fc12e2b851dd6a8de826f1297a265427df004b
Funto <funto.afuwape@gmail.com>
2023-01-11 13:30:35 +0100

    Create function to show input values on display
    Here, a basic function to show the input values of buttons on the display when clicked was created.
    
    All buttons, except the clear button, show on the display when clicked. The clear button clears the display.

--- calculator.js
+++ calculator.js
@@ -1,3 +1,6 @@
+const display = document.getElementById('calc-display')
+
+
 function add(...num) {
     return num.reduce((total, currentNum) => {
         return total + currentNum;
@@ -26,10 +29,20 @@ function operate(operator, num1, num2) {
     if (operator === '+'){
         return add(num1, num2);
     } else if (operator === '-'){
-        return subtract(num1, num2)
+        return subtract(num1, num2);
     } else if (operator === '/'){
-        return divide(num1, num2)
+        return divide(num1, num2);
     } else if (operator === '*'){
-        return multiply(num1, num2)
+        return multiply(num1, num2);
     }
 }
+
+document.addEventListener('click', e => {
+    if (e.target.id === 'clr-btn') {
+        display.innerHTML = ''
+    }
+    else if (e.target.nodeName === 'BUTTON') {
+        display.innerText += e.target.textContent
+    }
+})
+
